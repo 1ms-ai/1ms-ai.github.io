6f94db332c1671cffae362382b8bd676e789b6ad
haixuanTao <tao.xavier@outlook.com>
2024-04-28 10:51:31 +0200

    Adding new logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,11 +49,7 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "1ms.ai",
-      // logo: {
-      // alt: "My Site Logo",
-      // src: "img/logo.svg",
-      // },
+      title: "⚡ 1ms.ai",
       items: [
         {
           label: "Products",
